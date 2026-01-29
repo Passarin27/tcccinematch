1bc7eadcdbbad695981b42f6a016f9fc80807139
Passarin27 <gsilvasouza116658@gmail.com>
2026-01-29 16:48:47 -0300

    Delete Cinematch-Front directory

--- Cinematch-Front/index.js
+++
@@ -1,7 +0,0 @@
-function abrirLogin() {
-  document.getElementById("modalLogin").style.display = "flex";
-}
-
-function fecharLogin() {
-  document.getElementById("modalLogin").style.display = "none";
-}
