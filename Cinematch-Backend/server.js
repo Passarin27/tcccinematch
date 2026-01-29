0d73711e1405245d0f2fa3befa4c27afda55a1ae
Passarin27 <gsilvasouza116658@gmail.com>
2026-01-29 13:39:00 -0300

    Add files via upload

---
+++ Cinematch-Backend/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+console.log('JWT_SECRET:', process.env.JWT_SECRET);
+console.log('Servidor iniciando...');
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'API CineMatch rodando 🚀' });
+});
+
+app.use('/auth', require('./routes/auth.routes'));
+app.use('/users', require('./routes/users.routes'));
+app.use('/filmes', require('./routes/filmes.routes'));
+app.use('/listas', require('./routes/listas.routes'));
+
+/* =========================
+   START SERVER (RENDER)
+========================= */
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+});
