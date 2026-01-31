fb58f687ef60baf11f441906282ab72a094d85b2
Passarin27 <gsilvasouza116658@gmail.com>
2026-01-30 21:15:06 -0300

    Add files via upload

---
+++ Cinematch-Front/supabase.js
@@ -0,0 +1,4 @@
+const supabase = supabase.createClient(
+  "https://SUA_URL.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhubXJqa2l5bHR5emprZnluYW50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwOTc0NjUsImV4cCI6MjA4NDY3MzQ2NX0.PKbjlgZBVf254uUbmANdY3te2DkiIK3M3YVblMjkbP4"
+);
