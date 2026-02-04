74cc6a7e49edf72f5b9b6cb0d97c550747c24aad
Passarin27 <gsilvasouza116658@gmail.com>
2026-02-04 17:09:38 -0300

    Add files via upload

---
+++ Cinematch-Backend/src/generosTMDB.js
@@ -0,0 +1,23 @@
+const MAPA_GENEROS_TMDB = {
+  acao: 28,
+  aventura: 12,
+  animacao: 16,
+  comedia: 35,
+  crime: 80,
+  documentario: 99,
+  drama: 18,
+  familia: 10751,
+  fantasia: 14,
+  historia: 36,
+  terror: 27,
+  musica: 10402,
+  misterio: 9648,
+  romance: 10749,
+  ficcao_cientifica: 878,
+  filme_tv: 10770,
+  suspense: 53,
+  guerra: 10752,
+  western: 37
+};
+
+module.exports = MAPA_GENEROS_TMDB;
